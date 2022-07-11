ba522375dbf2fa8d4c6e5db5e0ec9cd1796c6132
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-07-11 17:07:27 -0400

    Parent components - ready to add the Routing part

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,23 @@
 import '../../App.css';
+import ErrorPage from '../ErrorPage/ErrorPage';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Landing from '../Landing/Landing';
+import Login from '../User/Login/Login';
+import Signup from '../User/Signup/Signup';
+import Welcome from '../Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+
+      <Welcome/>
       <Landing/>
+      <Login/>
+      <Signup/>
+      <ErrorPage/>
+
       <Footer/>
     </div>
   );
