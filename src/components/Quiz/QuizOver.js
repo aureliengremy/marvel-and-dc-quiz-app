d8d9444fafe40c8d05ab2599965256bdd53ec51c
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-07-25 14:55:29 -0400

    add modal to QuizOver

--- src/components/Quiz/QuizOver.js
+++ src/components/Quiz/QuizOver.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import { GiTrophyCup, GiPodiumWinner } from 'react-icons/gi'
 import Loader from '../Loader/Loader';
+import Modal from '../Modal/Modal';
 
 
 const QuizOver = React.forwardRef((props, ref) => {
@@ -8,16 +9,17 @@ const QuizOver = React.forwardRef((props, ref) => {
     // console.log(ref)
 
     const {
-        levelName, 
-        percent, 
-        score, 
-        maxQuestion, 
+        levelName,
+        percent,
+        score,
+        maxQuestion,
         quizLevelSelect,
         loadLevelQuestion
-    } 
-    = props;
+    }
+        = props;
 
     const [asked, setAsked] = useState([])
+    const [openModal, setOpenModal] = useState(false)
 
     // console.log(asked)
 
@@ -27,58 +29,67 @@ const QuizOver = React.forwardRef((props, ref) => {
 
     const averageScore = maxQuestion / 2
 
-    if(score < averageScore) {
+    if (score < averageScore) {
         setTimeout(() => {
             loadLevelQuestion(quizLevelSelect)
-        },5000)
+        }, 5000)
+    }
+
+    const showModal = (id) => {
+        setOpenModal(true)
+    }
+
+    const hideModal = () => {
+        setOpenModal(false)
     }
 
     const decision = score >= averageScore ? (
         <Fragment>
             <div className="stepsBtnContainer">
 
-            {
-                quizLevelSelect < levelName.length ? (
-                    <Fragment>
-                        <p className="successMsg"> <GiTrophyCup size='50px'/> Bravo, passez au niveau suivant !</p>
-                        <button className="btnResult success" onClick={() => loadLevelQuestion(quizLevelSelect)}>Niveau suivant</button>
-                    </Fragment>
-                ) : (
-                    <Fragment>
-                        <p className="successMsg"><GiPodiumWinner size="50px"/> Bravo, vous êtes un expert !</p>
-                        <button className="btnResult info" onClick={() => loadLevelQuestion(0)}>Acceuil</button>
-                    </Fragment>
-                )    
-            }
+                {
+                    quizLevelSelect < levelName.length ? (
+                        <Fragment>
+                            <p className="successMsg"> <GiTrophyCup size='50px' /> Bravo, passez au niveau suivant !</p>
+                            <button className="btnResult success" onClick={() => loadLevelQuestion(quizLevelSelect)}>Niveau suivant</button>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <p className="successMsg"><GiPodiumWinner size="50px" /> Bravo, vous êtes un expert !</p>
+                            <button className="btnResult info" onClick={() => loadLevelQuestion(0)}>Acceuil</button>
+                        </Fragment>
+                    )
+                }
             </div>
             <div className="percentage">
                 <div className="progressPercent">Réussite: {percent}%</div>
                 <div className="progressPercent">Note: {score}/{maxQuestion}</div>
             </div>
         </Fragment>
-        ) : (
+    ) : (
         <Fragment>
-             <div className="stepsBtnContainer">
+            <div className="stepsBtnContainer">
                 <p className="failureMsg">Dommage, vous n'avez pas ce qu'il faut !</p>
-             </div>
+            </div>
             <div className="percentage">
                 <div className="progressPercent">Réussite: {percent}%</div>
                 <div className="progressPercent">Note: {score}/{maxQuestion}</div>
             </div>
         </Fragment>
-        )
+    )
 
 
-    
     const questionAnswer = score >= averageScore ? (
-        
+
         asked.map(question => {
             return (
                 <tr key={question.id}>
                     <td>{question.question}</td>
                     <td>{question.answer}</td>
                     <td>
-                        <button className="btnInfo">Infos</button>
+                        <button className="btnInfo"
+                            onClick={() => showModal(question.heroId)}>Infos</button>
+
                     </td>
                 </tr>
             )
@@ -86,7 +97,7 @@ const QuizOver = React.forwardRef((props, ref) => {
     ) : (
         <tr >
             <td colSpan="3">
-                <Loader loadingMsg={'Pas de reponse !'} styling={{textAlign: 'center', color: 'red'}}/>
+                <Loader loadingMsg={'Pas de reponse !'} styling={{ textAlign: 'center', color: 'red' }} />
             </td>
         </tr>
     )
@@ -95,7 +106,7 @@ const QuizOver = React.forwardRef((props, ref) => {
 
     return (
         <Fragment>
-            {decision}   
+            {decision}
             <hr />
             <p>Les réponses aux question posées: </p>
             <div className="answerContianer">
@@ -112,6 +123,17 @@ const QuizOver = React.forwardRef((props, ref) => {
                     </tbody>
                 </table>
             </div>
+            <Modal showModal={openModal} hideModal={hideModal}>
+                <div className="modalHeader">
+                    <h2>Titre</h2>
+                </div>
+                <div className="modalBody">
+                    <h3>Titre 2</h3>
+                </div>
+                <div className="modalFooter">
+                    <button className="modalBtn">Fermer</button>
+                </div>
+            </Modal>
         </Fragment>
 
     )
