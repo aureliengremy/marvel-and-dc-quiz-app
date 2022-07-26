fe7bcd6a2f59e820b61c455cf13c36645dbb97e7
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-07-26 14:54:17 -0400

    add MarvelAPI - secure call and display data on Modal - key in .env

--- src/components/Quiz/QuizOver.js
+++ src/components/Quiz/QuizOver.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Fragment, useEffect, useState } from 'react'
 import { GiTrophyCup, GiPodiumWinner } from 'react-icons/gi'
 import Loader from '../Loader/Loader';
@@ -15,8 +16,12 @@ const QuizOver = React.forwardRef((props, ref) => {
         maxQuestion,
         quizLevelSelect,
         loadLevelQuestion
-    }
-        = props;
+    } = props;
+
+    const MarvelAPI_Public = process.env.REACT_APP_MARVEL_API
+    const hashAPI = "f3024a646ed065e0f068776862b41381"
+    const [characterInfo, setCharacterInfo] = useState([])
+    const [loadingInfo, setLoadingInfo] = useState(true)
 
     const [asked, setAsked] = useState([])
     const [openModal, setOpenModal] = useState(false)
@@ -25,7 +30,21 @@ const QuizOver = React.forwardRef((props, ref) => {
 
     useEffect(() => {
         setAsked(ref.current)
+        if (localStorage.getItem('marvelStorageDate')) {
+            const date = localStorage.getItem('marvelStorageDate')
+            checkDataAge(date)
+        }
     }, [ref])
+
+    const checkDataAge = date => {
+        let today = Date.now()
+        const timeDiff = today - date
+        const daysDiff = timeDiff / (1000 * 3600 * 24)
+        if (daysDiff >= 15) {
+            localStorage.clear()
+            localStorage.setItem('marvelStorageDate', Date.now())
+        }
+    }
 
     const averageScore = maxQuestion / 2
 
@@ -37,10 +56,37 @@ const QuizOver = React.forwardRef((props, ref) => {
 
     const showModal = (id) => {
         setOpenModal(true)
+
+        if (localStorage.getItem(id)) {
+            setCharacterInfo(JSON.parse(localStorage.getItem(id)))
+            setLoadingInfo(false)
+        } else {
+            // Make a request for a user with a given ID
+            axios.get(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${MarvelAPI_Public}&hash=${hashAPI}`)
+                .then((response) => {
+                    // console.log(response);
+                    setCharacterInfo(response.data)
+                    setLoadingInfo(false)
+
+                    localStorage.setItem(id, JSON.stringify(response.data))
+                    if (localStorage.getItem('marvelStorageDate')) {
+                        localStorage.setItem('marvelStorageDate', Date.now())
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
+
     }
 
     const hideModal = () => {
         setOpenModal(false)
+        setLoadingInfo(true)
+    }
+
+    const capitalizedLink = text => {
+        return text.charAt(0).toUpperCase() + text.slice(1)
     }
 
     const decision = score >= averageScore ? (
@@ -102,7 +148,50 @@ const QuizOver = React.forwardRef((props, ref) => {
         </tr>
     )
 
-
+    const resultInModal = loadingInfo ? (
+        <Fragment>
+            <div className="modalHeader">
+                <h2>Réponse de Marvel ...</h2>
+            </div>
+            <div className="modalFooter">
+                <button className="modalBtn">Fermer</button>
+            </div>
+        </Fragment>
+    ) : (
+        <Fragment>
+            <div className="modalHeader">
+                <h2>{characterInfo.data.results[0].name}</h2>
+            </div>
+            <div className="modalBody">
+                <div className="comicImage">
+                    <img
+                        src={characterInfo.data.results[0].thumbnail.path + '.' + characterInfo.data.results[0].thumbnail.extension}
+                        alt={characterInfo.data.results[0].name} />
+                    {characterInfo.attributionText}
+                </div>
+                <div className="comicDetails">
+                    <h3>Description</h3>
+                    <p>{characterInfo.data.results[0].description ?
+                        characterInfo.data.results[0].description :
+                        'Description indisponible ...'}</p>
+                    <h3>Plus d'infos</h3>
+                    {characterInfo.data.results[0].urls &&
+                        characterInfo.data.results[0].urls.map((link, index) => {
+                            return <a key={index}
+                                href={link.url}
+                                className="modalBtn"
+                                target="_blank"
+                                rel="noreferrer">
+                                {capitalizedLink(link.type)}
+                            </a>
+                        })}
+                </div>
+            </div>
+            <div className="modalFooter">
+                <button className="modalBtn" onClick={hideModal}>Fermer</button>
+            </div>
+        </Fragment>
+    )
 
     return (
         <Fragment>
@@ -124,15 +213,7 @@ const QuizOver = React.forwardRef((props, ref) => {
                 </table>
             </div>
             <Modal showModal={openModal} hideModal={hideModal}>
-                <div className="modalHeader">
-                    <h2>Titre</h2>
-                </div>
-                <div className="modalBody">
-                    <h3>Titre 2</h3>
-                </div>
-                <div className="modalFooter">
-                    <button className="modalBtn">Fermer</button>
-                </div>
+                {resultInModal}
             </Modal>
         </Fragment>
 
